8c8e82d71135d8c0637e177ca8317b79fcd2110f
valentine ollawa <vollawa@gmail.com>
2013-09-24 21:21:45 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trft::Application.config.secret_token = 'da9167cc96a8158142885635a4ddb619ab5c1ffc411c8eba5d0220d952539d0dae8f120e9789308e69b16515103c136b1487c2fe268f3d5ec2ab4efce7b0afab'
